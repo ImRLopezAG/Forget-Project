9a9e70a900dd487d82db27b7df7152d8284122c1
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-05-04 17:27:53 -0400

    update build

--- Forget-Api-Ts/dist/src/middleware/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, _req, res, _next) => {
-    return res.status(500).json({ status: 500, message: err.message });
-};
-exports.errorHandler = errorHandler;
--- Forget-Api-Ts/dist/src/routes/auth.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("../auth/auth.controller");
-exports.auth = (0, express_1.Router)();
-exports.auth.post('/Login', auth_controller_1.authenticate);
