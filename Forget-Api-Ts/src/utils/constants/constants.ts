f144e4fe36c231240d31ea125197ba892d570336
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-05-07 21:11:39 -0400

    update constants

--- Forget-Api-Ts/src/utils/constants/constants.ts
+++ Forget-Api-Ts/src/utils/constants/constants.ts
@@ -5,5 +5,5 @@ dotenv.config()
 export const PORT = process.env.PORT as string
 export const BASE = process.env.BASE as string
 export const SECRET = process.env.JWT_SECRET as string
-export const MONGODB_URI = process.env.MONGO_URI as string
+export const MONGODB_URI = process.env.MONGODB_URI as string
 export const DB_NAME = process.env.DB_NAME as string
